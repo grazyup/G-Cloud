ef734021378a829c1d9ac92dfdcbbfd4de5a6cf3
grazy <grazyup@163.com>
2024-01-26 01:22:50 +0800

    更新Swagger2ConfigurationProperties中获取配置文件信息的方式

--- framwork/swagger2/src/main/java/com/grazy/swagger2/Swagger2ConfigProprieties.java
+++ framwork/swagger2/src/main/java/com/grazy/swagger2/Swagger2ConfigProprieties.java
@@ -1,7 +1,6 @@
 package com.grazy.swagger2;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -12,37 +11,28 @@ import org.springframework.stereotype.Component;
  */
 
 @Component
+//自动装配配置文件中前缀为swagger2中的属性值，可省略@Value注解获取值
 @ConfigurationProperties(prefix = "swagger2")
 @Data
 public class Swagger2ConfigProprieties {
 
-    @Value("${swagger2.show}")
     private boolean show;
 
-    @Value("${swagger2.group-name}")
     private String groupName;
 
-    @Value("${swagger2.title}")
     private String title;
 
-    @Value("${swagger2.base-package}")
     private String basePackage;
 
-    @Value("${swagger2.description}")
     private String description;
 
-    @Value("${swagger2.terms-of-service-url}")
     private String termsOfServiceUrl;
 
-    @Value("${swagger2.contact-name}")
     private String ContactName;
 
-    @Value("${swagger2.contact-url}")
     private String ContactUrl;
 
-    @Value("${swagger2.contact-email}")
     private String ContactEmail;
 
-    @Value("${swagger2.version}")
     private String Version;
 }
